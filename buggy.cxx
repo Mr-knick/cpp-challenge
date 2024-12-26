46d9f63fdc9f114756d11fac884c1ee590a2ed32
Brent Knickerbocker <brentknickerbocker@gmail.com>
2024-12-26 06:35:45 +0000

    Cleaned up code and have the most basic version of the requirements completed.
    Will accept only one input per line. Note does not account for words with spaces and drops inputs after the first word's space.
    
    Uses a map to maintin alphabetic order and quick addition and removal of words. Uses a queue to add words to be processed into map(a little overkill but so is the thread itself). Mutex's added to ensure thread safety. Also added delete to clean up thread.
    
    Next move is to change it to break up files, switch out to smart pointers, and add testing.

--- buggy.cxx
+++ buggy.cxx
@@ -1,93 +1,42 @@
+#include <iostream>
 #include <thread>
 #include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-
-#include <iostream>
+#include <map>
 #include <queue>
 #include <string>
 
-// struct Word
-// {
-//   char * data;
-//   int count;
-//
-//   Word (const char * data_ ) :
-//     data( ::strdup(data_) )
-//   {}
-//
-//   Word () :
-//     data((char *)"")
-//   {}
-// };
-
-// std::vector<Word*> s_wordsArray;
-// Word s_word;
-// int s_totalFound;
-
-std::vector<std::string> words;
 std::queue<std::string> inputWordsQueue{};
 std::mutex inputMutex{};
+//std::map automatically sorts keys/words
+std::map<std::string, int> words{};
+int totalFound = 0;
 
 // Worker thread: consume words passed from the main thread and insert them
-// in the 'word list' (s_wordsArray), while removing duplicates. Terminate when
+// in the 'word list' (words), while removing duplicates. Terminate when
 // the word 'end' is encountered.
 static void workerThread (std::atomic_bool& endEncountered)
 {
-  bool found = false;
-  std::cout << "begin process" << std::endl;
   while (!endEncountered) {
     if (!inputWordsQueue.empty()){
-      std::cout << "process one word" << std::endl;
       inputMutex.lock();
-      std::cout << "process locked" << std::endl;
       std::string currentWord = inputWordsQueue.front();
       inputWordsQueue.pop();
       if (currentWord == "end") {
         endEncountered = true;
         inputMutex.unlock();
-        std::cout << "unlocked process" << std::endl;
         break;
       }
-      std::cout << "push back word" << std::endl;
-      words.push_back(currentWord);
+      auto it = words.find(currentWord);
+      if (it == words.end()) {
+        words.insert({currentWord, 1});
+      }
+      else {
+        it->second++;
+      }
       inputMutex.unlock();
-      std::cout << "unlocked process" << std::endl;
     }
-
-
   }
-  std::cout << "now end" << std::endl;
-  // while (!endEncountered)
-  // {
-  //   if (s_word.data[0]) // Do we have a new word?
-  //   {
-  //     Word * w = new Word(s_word); // Copy the word
-  //
-  //     s_word.data[0] = 0; // Inform the producer that we consumed the word
-  //
-  //     endEncountered = std::strcmp( s_word.data, "end" ) == 0;
-  //
-  //     if (!endEncountered)
-  //     {
-  //       // Do not insert duplicate words
-  //       for ( auto p : s_wordsArray )
-  //       {
-  //         if (!std::strcmp( p->data, w->data ))
-  //         {
-  //           ++p->count;
-  //           found = true;
-  //           break;
-  //         }
-  //       }
-  //
-  //       if (!found)
-  //         s_wordsArray.push_back( w );
-  //     }
-  //   }
-  // }
-};
+}
 
 // Read input words from STDIN and pass them to the worker thread for
 // inclusion in the word list.
@@ -97,39 +46,32 @@ static void readInputWords ()
 {
   std::atomic_bool endEncountered = false;
   std::string inputWord;
-  std::thread * worker = new std::thread( workerThread , std::ref(endEncountered));
+  auto * worker = new std::thread( workerThread , std::ref(endEncountered));
+
+  // If there is a need to have words that contain spaces use the following snippet.
+  // ie. last names with a space. Or build custom solution in std. ie range or for loop
+  // #include <boost/algorithm/string.hpp>
+  // std::getline(std::cin, inputWord);
+  // boost::algorithm::trim(inputWord);
 
   while (!endEncountered) {
-    std::cout << "begin input" << std::endl;
+    // By default cin will only do one word at a time
     std::cin >> inputWord;
+
+    // Clear any additional information that may have been input on the line.
+    // This will ensure only one word is accepted at a time
+    std::cin.clear();
+    std::cin.ignore(INT_MAX, '\n');
+
     inputMutex.lock();
     inputWordsQueue.push(inputWord);
     inputMutex.unlock();
-    if (inputWord == "end") break;
-    std::cout << "unlock input" << std::endl;
+    if (inputWord == "end") {
+      break;
+    }
   }
-
-  std::cout << "test" << std::endl;
   worker->join();
-
-
-
-
-  // char * linebuf = new char[32];
-  //
-  // while (!endEncountered)
-  // {
-  //   if (!std::gets( linebuf )) // EOF?
-  //     return;
-  //
-  //   endEncountered = std::strcmp( linebuf, "end" ) == 0;
-  //
-  //   // Pass the word to the worker thread
-  //   std::strcpy( s_word.data, linebuf );
-  //   while (s_word.data[0]); // Wait for the worker thread to consume it
-  // }
-  //
-  // worker->join(); // Wait for the worker to terminate
+  delete worker;
 }
 
 // Repeatedly ask the user for a word and check whether it was present in the word list
@@ -137,47 +79,26 @@ static void readInputWords ()
 //
 static void lookupWords ()
 {
-  bool found;
-  // char * linebuf = new char[32];
   std::string lookupWord{};
 
   for(;;) {
-    std::printf( "\nEnter a word for lookup:" );
+    std::cout << "\nEnter a word for lookup:" << std::endl;
     std::cin >> lookupWord;
     if (std::cin.eof()) {
       // NOTE EOF will behave differently depending on if ran with IDE and terminal
       std::cout << "EOF detected" << std::endl;
       return;
     }
-    std::printf("\nHere %s", lookupWord.c_str());
-
-
-    // // Initialize the word to search for
-    // Word * w = new Word();
-    // std::strcpy( w->data, linebuf );
-
-    // // Search for the word
-    // unsigned i;
-    // for ( i = 0; i < s_wordsArray.size(); ++i )
-    // {
-    //   if (std::strcmp( s_wordsArray[i]->data, w->data ) == 0)
-    //   {
-    //     found = true;
-    //     break;
-    //   }
-    // }
-    // Uses a map
-
+    auto it = words.find(lookupWord);
+    if (it != words.end()) {
+      std::cout << "SUCCESS: " << lookupWord << " was present " << std::to_string(it->second) <<
+        " times in the initial word list" << std::endl;
+      totalFound++;
+    }
+    else {
+      std::cout << lookupWord << "was NOT found in the initial word list" << std::endl;
+    }
   }
-  //   if (found)
-  //   {
-  //     std::printf( "SUCCESS: '%s' was present %d times in the initial word list\n",
-  //                  s_wordsArray[i]->data, s_wordsArray[i]->count );
-  //     ++s_totalFound;
-  //   }
-  //   else
-  //     std::printf( "'%s' was NOT found in the initial word list\n", w->data );
-  // }
 }
 
 int main ()
@@ -185,23 +106,19 @@ int main ()
   try
   {
     readInputWords();
-    
-    // Sort the words alphabetically
-    // use ordered map
-    // std::sort( s_wordsArray.begin(), s_wordsArray.end() );
 
     // Print the word list
-    std::printf( "\n=== Word list:\n" );
-    // for ( auto p : s_wordsArray )
-    //   std::printf( "%s %d\n", p->data, p->count );
+    std::cout << "\n=== Word list:" << std::endl;
+    for (const auto& wordAndCount: words) {
+      std::cout << wordAndCount.first << " " << wordAndCount.second << std::endl;
+    }
 
     lookupWords();
-    //
-    // printf( "\n=== Total words found: %d\n", s_totalFound );
+    std::cout << "\n=== Total words found: " << std::to_string(totalFound) << std::endl;
   }
   catch (std::exception & e)
   {
-    std::printf( "error %s\n", e.what() );
+    std::cout << "error " << e.what() << std::endl;
   }
   
   return 0;
